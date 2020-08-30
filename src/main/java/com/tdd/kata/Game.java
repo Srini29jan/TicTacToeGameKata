a0f2cc5851974b409d493ef711dc0dcd4c3d3f21
Srinivas Subburaj <ssrinivas29jan@gmail.com>
2020-08-31 00:31:40 +0530

    extracted constants GRID_SIZE and PLAYER_X

--- src/main/java/com/tdd/kata/Game.java
+++ src/main/java/com/tdd/kata/Game.java
@@ -2,7 +2,9 @@ package com.tdd.kata;
 
 public class Game {
 
-    private final char[][] board = new char[3][3];
+    private static final int GRID_SIZE = 3;
+    private static final char PLAYER_X = 'X';
+    private final char[][] board = new char[GRID_SIZE][GRID_SIZE];
     private char previousPlayer;
 
     public char getPlayerAt(int row, int column) {
@@ -10,7 +12,7 @@ public class Game {
     }
 
     public void playAt(int row, int column) {
-        char currentPlayer = previousPlayer == 'X' ? 'O' : 'X';
+        char currentPlayer = previousPlayer == PLAYER_X ? 'O' : PLAYER_X;
         board[row][column] = currentPlayer;
         previousPlayer = currentPlayer;
     }
