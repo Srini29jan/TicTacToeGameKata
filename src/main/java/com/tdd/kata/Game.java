395848872da5ec95d816a45f6ce988c8b1386771
Srinivas Subburaj <ssrinivas29jan@gmail.com>
2020-09-01 08:55:48 +0530

    extracted method isThirdRowFilledByPlayerO

--- src/main/java/com/tdd/kata/Game.java
+++ src/main/java/com/tdd/kata/Game.java
@@ -45,13 +45,17 @@ public class Game {
             winner = PLAYER_X;
         }
 
-        if (getPlayerAt(POSITION_TWO, POSITION_ZERO) == PLAYER_O
-                && getPlayerAt(POSITION_TWO, POSITION_ONE) == PLAYER_O
-                && getPlayerAt(POSITION_TWO, POSITION_TWO) == PLAYER_O) {
-            return PLAYER_O;
+        if (isThirdRowFilledByPlayerO()) {
+            winner = PLAYER_O;
         }
 
         return winner;
+    }
+
+    private boolean isThirdRowFilledByPlayerO() {
+        return getPlayerAt(POSITION_TWO, POSITION_ZERO) == PLAYER_O
+                && getPlayerAt(POSITION_TWO, POSITION_ONE) == PLAYER_O
+                && getPlayerAt(POSITION_TWO, POSITION_TWO) == PLAYER_O;
     }
 
     private boolean isThirdRowFilledByPlayerX() {
