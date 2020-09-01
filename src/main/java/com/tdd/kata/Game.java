61b1bc2c24a6ac186cfd5c53ddd67b5e316e4c37
Srinivas Subburaj <ssrinivas29jan@gmail.com>
2020-09-01 11:24:51 +0530

    verified if player O is winner if O fills top right to bottom left diagonal

--- src/main/java/com/tdd/kata/Game.java
+++ src/main/java/com/tdd/kata/Game.java
@@ -47,6 +47,12 @@ public class Game {
             winner = PLAYER_X;
         }
 
+        if (getPlayerAt(POSITION_ZERO, POSITION_TWO) == PLAYER_O
+                && getPlayerAt(POSITION_ONE, POSITION_ONE) == PLAYER_O
+                && getPlayerAt(POSITION_TWO, POSITION_ZERO) == PLAYER_O) {
+            winner = PLAYER_O;
+        }
+
         return winner;
     }
 
